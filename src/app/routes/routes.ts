713de61afc347ea4bb27b814b4e05b7d3efd6464
ali gamal <a.gamal@sieraj.com>
2018-12-16 14:13:19 +0300

    add header with new design

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -4,15 +4,14 @@ import { DayCalendarComponent } from "../CalendarComponents/day-calendar/day-cal
 import { NotFoundComponent } from "../not-found/not-found.component";
 import { SignUpComponent } from "../AuthenticationComponent/sign-up/sign-up.component";
 
-
 export const appRoutes: Routes = [
-  { path: '', component: SignUpComponent },
-  //{ path: '', redirectTo: '/homeCalendar', pathMatch: 'full' },
-  { path: 'homeCalendar', component: HomeCalendarComponent },
-  { path: 'dayCalendar/:date', component: DayCalendarComponent },
+  // { path: '', component: SignUpComponent },
+  { path: "", redirectTo: "/homeCalendar", pathMatch: "full" },
+  { path: "homeCalendar", component: HomeCalendarComponent },
+  { path: "dayCalendar/:date", component: DayCalendarComponent },
   //{path:'dayCalendar/:model', component:DayCalendarComponent},
   // {path:'components/Admin/approve', component:ApproveAppointmentComponent},
   // {path:'components/Admin/edit', component:EditAppointmentComponent},
   // {path:'components/Admin/cancel', component:CancelAppointmentComponent}
-  { path: '**', component: NotFoundComponent }
+  { path: "**", component: NotFoundComponent }
 ];
