85e328555641a817b1d51514d1c12e51bfe24ef3
ali gamal <a.gamal@sieraj.com>
2018-12-30 13:33:06 +0300

    finish calendar localization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,48 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from './AuthenticationComponent/sign-in/sign-in.component';
-import { SignUpComponent } from './AuthenticationComponent/sign-up/sign-up.component';
-import { HomeCalendarComponent } from './CalendarComponents/home-calendar/home-calendar.component';
-import { DayCalendarComponent } from './CalendarComponents/day-calendar/day-calendar.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AuthGuard } from './guards/auth.guard';
-import { TestComponent } from './Admin/test/test.component';
-import { ForbiddenComponent } from './AuthenticationComponent/forbidden/forbidden.component';
-import { IndexComponent } from './CalendarComponents/index/index.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SignInComponent } from "./AuthenticationComponent/sign-in/sign-in.component";
+import { SignUpComponent } from "./AuthenticationComponent/sign-up/sign-up.component";
+import { HomeCalendarComponent } from "./CalendarComponents/home-calendar/home-calendar.component";
+import { DayCalendarComponent } from "./CalendarComponents/day-calendar/day-calendar.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { TestComponent } from "./Admin/test/test.component";
+import { ForbiddenComponent } from "./AuthenticationComponent/forbidden/forbidden.component";
+import { IndexComponent } from "./CalendarComponents/index/index.component";
 //import { RegisterComponent } from './AuthenticationComponent/register/register.component';
 
-
-const appRoutes : Routes = [
-  { path: '', component: SignInComponent },
+const appRoutes: Routes = [
+  // { path: "", component: SignInComponent },
   //{path:'register', component:RegisterComponent},
-  { path: 'signUp', component: SignUpComponent },
-  { path: 'index', component:IndexComponent},
-  {path: 'forbidden', component: ForbiddenComponent,canActivate: [AuthGuard]},
-  { path: 'test', component: TestComponent,canActivate:[AuthGuard],data: {roles: ['SuperAdmin','Admin']} },
-  { path: 'homeCalendar', component: HomeCalendarComponent,canActivate:[AuthGuard] },
-  { path: 'dayCalendar/:date', component: DayCalendarComponent,canActivate:[AuthGuard] },
+  { path: "signUp", component: SignUpComponent },
+  { path: "index", component: IndexComponent },
+  {
+    path: "forbidden",
+    component: ForbiddenComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "test",
+    component: TestComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ["SuperAdmin", "Admin"] }
+  },
+  {
+    path: "",
+    component: HomeCalendarComponent
+    // canActivate: [AuthGuard]
+  },
+  {
+    path: "dayCalendar/:date",
+    component: DayCalendarComponent
+    // canActivate: [AuthGuard]
+  },
   { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes),
-  ],
-  providers:[AuthGuard],
+  imports: [RouterModule.forRoot(appRoutes)],
+  providers: [AuthGuard],
   exports: [RouterModule]
- 
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
