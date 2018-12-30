dc8353d35001df31049b3f5a744a70355da87a23
ali gamal <a.gamal@sieraj.com>
2018-12-30 10:48:40 +0300

    apply UI on login page

--- src/app/AuthenticationComponent/sign-in/sign-in.component.ts
+++ src/app/AuthenticationComponent/sign-in/sign-in.component.ts
@@ -1,69 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { LoginUser } from '../../models/loginUser.model';
-import { HttpErrorResponse } from '@angular/common/http';
-import { AuthService } from '../../services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { LoginUser } from "../../models/loginUser.model";
+import { HttpErrorResponse } from "@angular/common/http";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  selector: "sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.css"]
 })
 export class SignInComponent implements OnInit {
-    loginUser : LoginUser;
-    userClaims : any;
-    isLoginError : boolean = false;
-    
+  loginUser: LoginUser;
+  userClaims: any;
+  isLoginError: boolean = false;
 
-    constructor(
-       // private userService : UserService,
-        private authService : AuthService,
-         private router: Router
+  constructor(
+    // private userService : UserService,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginUser = new LoginUser("", "");
+  }
 
-        ) {
-            this.loginUser = new LoginUser('','');
-        }
+  ngOnInit() {
+    // this.authService.getUserClaims().subscribe((data : any)=>{
+    //     this.userClaims = data;
+    // });
+  }
 
-    ngOnInit() {
-        // this.authService.getUserClaims().subscribe((data : any)=>{
-        //     this.userClaims = data;
-        // });
-    }
+  // getUserCredentials(){
+  //     // this.userService.getUserClaims().subscribe((data : any)=>{
+  //         this.authService.getUserClaims().subscribe((data : any)=>{
+  //         this.userClaims = data;
+  //         localStorage.setItem('userCredentials',this.userClaims);
+  //         console.log(this.userClaims);
+  //     });
+  // }
 
-    // getUserCredentials(){
-    //     // this.userService.getUserClaims().subscribe((data : any)=>{
-    //         this.authService.getUserClaims().subscribe((data : any)=>{
-    //         this.userClaims = data;
-    //         localStorage.setItem('userCredentials',this.userClaims);
-    //         console.log(this.userClaims);
-    //     });
-    // }
-
-    OnSubmit(username : string,password : string){
-        // this.userService.userAuthentication(username,password).subscribe((data : any)=>{
-        this.authService.login(username,password)
-        .subscribe((data : any)=>{
-        localStorage.setItem('userToken',data.access_token);
-        localStorage.setItem('userRoles',data.role);
-        this.router.navigate(['/homeCalendar']);
+  OnSubmit(username: string, password: string) {
+    // this.userService.userAuthentication(username,password).subscribe((data : any)=>{
+    this.authService.login(username, password).subscribe(
+      (data: any) => {
+        localStorage.setItem("userToken", data.access_token);
+        localStorage.setItem("userRoles", data.role);
+        this.router.navigate(["/homeCalendar"]);
         //neeche ka code tab use karenge jab index page design kar liye honge
         // console.log(data.role);
         // if(this.authService.roleMatch(['SuperAdmin']))
-        
+
         // {
         //     this.router.navigate(['/homeCalendar']);
         // }
-        // else{     
+        // else{
         // this.router.navigate(['/index']);
         // }
-        
-        },
-        (err : HttpErrorResponse)=>{
-            this.isLoginError = true;
-        });
-        
-    }
-
-    
-
+      },
+      (err: HttpErrorResponse) => {
+        this.isLoginError = true;
+      }
+    );
+  }
 }
